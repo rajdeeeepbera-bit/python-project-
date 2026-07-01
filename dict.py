d1a06adbc0d93afe93e189a0c62452e2af2528ee
rajdeeeepbera-bit <rajdeeeepbera@gmail.com>
2026-07-01 17:05:54 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,13 @@
+name = {"name1": "Rajdeep Bera","Roll no.": 19,
+    "sec": "A"}
+    
+x = (name["name1"])
+print(x)
+if x == "Rajdeep":
+	print("Hello Rajdeep")
+
+elif x == "Rajdeep Bera":
+	print("Hello Rajdeep")	
+
+else:
+	print("Hi") 
